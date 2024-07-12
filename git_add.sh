211b38c9d21de165d6902ee059c448bb8c8526fc
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-07-12 21:45:38 +0900

    fix: localを削除

--- git_add.sh
+++ git_add.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ]; then
   git add .
 else
-  local cmd="git status -s | awk '{print \$2}'"
+  cmd="git status -s | awk '{print \$2}'"
   for arg in "$@"; do
     cmd="$cmd | grep '$arg'"
   done
